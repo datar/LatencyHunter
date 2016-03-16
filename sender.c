5b6c601c397ad4e2b1ac6d43993ee2948d946ec8
datar <tianxian@gmail.com>
2016-03-15 20:29:09 -0700

    Daily 20160315
    fix sender

--- sender.c
+++ sender.c
@@ -78,10 +78,43 @@ static void do_mcast(struct configuration* cfg, int sock, struct sockaddr_in* ad
 		       &imr, sizeof(struct ip_mreq)) < 0)
 		bail("join multicast group");
 
+	int ip_multicast_loop = 0;
 	if (setsockopt(sock, IPPROTO_IP, IP_MULTICAST_LOOP,
 		       &ip_multicast_loop, sizeof(enabled)) < 0) {
 		bail("loop multicast");
 	}
+}
+
+
+int set_socket_timestamp_options(int sock){
+
+	return 0;
+}
+
+int verify_socket_timestamp_options(int sock){
+	int val;
+	socklen_t len = sizeof(val);
+	if (getsockopt(sock, SOL_SOCKET, SO_TIMESTAMP, &val, &len) < 0)
+		DEBUG("%s: %s\n", "getsockopt SO_TIMESTAMP", strerror(errno));
+	else
+		DEBUG("SO_TIMESTAMP %d\n", val);
+
+	if (getsockopt(sock, SOL_SOCKET, SO_TIMESTAMPNS, &val, &len) < 0)
+		DEBUG("%s: %s\n", "getsockopt SO_TIMESTAMPNS",
+		       strerror(errno));
+	else
+		DEBUG("SO_TIMESTAMPNS %d\n", val);
+
+	if (getsockopt(sock, SOL_SOCKET, SO_TIMESTAMPING, &val, &len) < 0) {
+		DEBUG("%s: %s\n", "getsockopt SO_TIMESTAMPING",
+		       strerror(errno));
+	} else {
+		DEBUG("SO_TIMESTAMPING %d\n", val);
+		if (val != so_timestamping_flags)
+			DEBUG("   not the expected value %d\n",
+			       so_timestamping_flags);
+	}
+	return 0;
 }
 
 
@@ -91,7 +124,7 @@ int setup_socket(int sock, struct configuration cfg){
 	int so_timestampns = 0;
 	int siocgstamp = 0;
 	int siocgstampns = 0;
-	int ip_multicast_loop = 0;
+	
 	int i;
 	int enabled = 1;
 	
@@ -100,11 +133,6 @@ int setup_socket(int sock, struct configuration cfg){
 	struct hwtstamp_config hwconfig, hwconfig_requested;
 	struct sockaddr_in addr;
 	
-	
-	int val;
-	socklen_t len;
-	struct timeval next;
-
 	so_timestamping_flags |= SOF_TIMESTAMPING_TX_HARDWARE;
 	so_timestamping_flags |= SOF_TIMESTAMPING_RX_HARDWARE;
 	so_timestamping_flags |= SOF_TIMESTAMPING_RAW_HARDWARE;
@@ -176,28 +204,7 @@ int setup_socket(int sock, struct configuration cfg){
 		       &enabled, sizeof(enabled)) < 0)
 		DEBUG("%s: %s\n", "setsockopt IP_PKTINFO", strerror(errno));
 
-	// verify socket options 
-	len = sizeof(val);
-	if (getsockopt(sock, SOL_SOCKET, SO_TIMESTAMP, &val, &len) < 0)
-		DEBUG("%s: %s\n", "getsockopt SO_TIMESTAMP", strerror(errno));
-	else
-		DEBUG("SO_TIMESTAMP %d\n", val);
-
-	if (getsockopt(sock, SOL_SOCKET, SO_TIMESTAMPNS, &val, &len) < 0)
-		DEBUG("%s: %s\n", "getsockopt SO_TIMESTAMPNS",
-		       strerror(errno));
-	else
-		DEBUG("SO_TIMESTAMPNS %d\n", val);
-
-	if (getsockopt(sock, SOL_SOCKET, SO_TIMESTAMPING, &val, &len) < 0) {
-		DEBUG("%s: %s\n", "getsockopt SO_TIMESTAMPING",
-		       strerror(errno));
-	} else {
-		DEBUG("SO_TIMESTAMPING %d\n", val);
-		if (val != so_timestamping_flags)
-			DEBUG("   not the expected value %d\n",
-			       so_timestamping_flags);
-	}
+	
 
 	return 0;
 }
@@ -215,6 +222,7 @@ int sender(int argc, char **argv)
 
 
 	// send packets forever every five seconds 
+	//struct timeval next;
 	//gettimeofday(&next, 0);
 	//next.tv_sec = (next.tv_sec + 1) ;
 	//next.tv_usec = 0;
@@ -244,177 +252,3 @@ int sender(int argc, char **argv)
 */	
 	return 0;
 }
-
-
-
-/*
-int sender(int argc, char **argv)
-{
-	int so_timestamping_flags = 0;
-	int so_timestamp = 0;
-	int so_timestampns = 0;
-	int siocgstamp = 0;
-	int siocgstampns = 0;
-	int ip_multicast_loop = 0;
-	char *interface;
-	int i;
-	int enabled = 1;
-	int sock;
-	struct ifreq device;
-	struct ifreq hwtstamp;
-	struct hwtstamp_config hwconfig, hwconfig_requested;
-	struct sockaddr_in addr;
-	struct ip_mreq imr;
-	struct in_addr iaddr;
-	int val;
-	socklen_t len;
-	struct timeval next;
-
-	interface = "eth0";
-
-	so_timestamping_flags |= SOF_TIMESTAMPING_TX_HARDWARE;
-	//so_timestamping_flags |= SOF_TIMESTAMPING_TX_SOFTWARE;
-	so_timestamping_flags |= SOF_TIMESTAMPING_RX_HARDWARE;
-	so_timestamping_flags |= SOF_TIMESTAMPING_RAW_HARDWARE;
-
-	sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
-	if (sock < 0)
-		bail("socket");
-	int on = 1;
-	setsockopt( sock, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on) );
-
-	memset(&device, 0, sizeof(device));
-	strncpy(device.ifr_name, interface, sizeof(device.ifr_name));
-	if (ioctl(sock, SIOCGIFADDR, &device) < 0)
-		bail("getting interface IP address");
-
-	memset(&hwtstamp, 0, sizeof(hwtstamp));
-	strncpy(hwtstamp.ifr_name, interface, sizeof(hwtstamp.ifr_name));
-	hwtstamp.ifr_data = (void *)&hwconfig;
-	memset(&hwconfig, 0, sizeof(hwconfig));
-	
-
-	hwconfig.tx_type =
-		(so_timestamping_flags & SOF_TIMESTAMPING_TX_HARDWARE) ?
-		HWTSTAMP_TX_ON : HWTSTAMP_TX_OFF;
-	hwconfig.rx_filter =
-		(so_timestamping_flags & SOF_TIMESTAMPING_RX_HARDWARE) ?
-		//HWTSTAMP_FILTER_PTP_V1_L4_SYNC : HWTSTAMP_FILTER_NONE;
-		HWTSTAMP_FILTER_ALL : HWTSTAMP_FILTER_NONE;
-	hwconfig_requested = hwconfig;
-
-	
-	if (ioctl(sock, SIOCSHWTSTAMP, &hwtstamp) < 0) {
-		if ((errno == EINVAL || errno == ENOTSUP) &&
-		    hwconfig_requested.tx_type == HWTSTAMP_TX_OFF &&
-		    hwconfig_requested.rx_filter == HWTSTAMP_FILTER_NONE)
-			DEBUG("SIOCSHWTSTAMP: disabling hardware time stamping not possible\n");
-		else
-			bail("SIOCSHWTSTAMP");
-	}
-	DEBUG("SIOCSHWTSTAMP: tx_type %d requested, got %d; rx_filter %d requested, got %d\n", hwconfig_requested.tx_type, hwconfig.tx_type, hwconfig_requested.rx_filter, hwconfig.rx_filter);
-
-	
-	addr.sin_family = AF_INET;
-	addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	addr.sin_port = htons(321); //PTP event port 
-	if (bind(sock,
-		 (struct sockaddr *)&addr,
-		 sizeof(struct sockaddr_in)) < 0)
-		bail("bind");
-	
-	//* set multicast group for outgoing packets
-	inet_aton("11.11.11.2", &iaddr); 
-	addr.sin_addr = iaddr;
-        
-	imr.imr_multiaddr.s_addr = iaddr.s_addr;
-	imr.imr_interface.s_addr =
-		((struct sockaddr_in *)&device.ifr_addr)->sin_addr.s_addr;
-	if (setsockopt(sock, IPPROTO_IP, IP_MULTICAST_IF,
-		       &imr.imr_interface.s_addr, sizeof(struct in_addr)) < 0)
-		bail("set multicast");
-
-	//* join multicast group, loop our own packet 
-	if (setsockopt(sock, IPPROTO_IP, IP_ADD_MEMBERSHIP,
-		       &imr, sizeof(struct ip_mreq)) < 0)
-		bail("join multicast group");
-
-	if (setsockopt(sock, IPPROTO_IP, IP_MULTICAST_LOOP,
-		       &ip_multicast_loop, sizeof(enabled)) < 0) {
-		bail("loop multicast");
-	}
-
-	// set socket options for time stamping 
-	if (so_timestamp &&
-		setsockopt(sock, SOL_SOCKET, SO_TIMESTAMP,
-			   &enabled, sizeof(enabled)) < 0)
-		bail("setsockopt SO_TIMESTAMP");
-
-	if (so_timestampns &&
-		setsockopt(sock, SOL_SOCKET, SO_TIMESTAMPNS,
-			   &enabled, sizeof(enabled)) < 0)
-		bail("setsockopt SO_TIMESTAMPNS");
-
-	if (so_timestamping_flags &&
-		setsockopt(sock, SOL_SOCKET, SO_TIMESTAMPING,
-			   &so_timestamping_flags,
-			   sizeof(so_timestamping_flags)) < 0)
-		bail("setsockopt SO_TIMESTAMPING");
-
-	// request IP_PKTINFO for debugging purposes 
-	if (setsockopt(sock, SOL_IP, IP_PKTINFO,
-		       &enabled, sizeof(enabled)) < 0)
-		DEBUG("%s: %s\n", "setsockopt IP_PKTINFO", strerror(errno));
-
-	// verify socket options 
-	len = sizeof(val);
-	if (getsockopt(sock, SOL_SOCKET, SO_TIMESTAMP, &val, &len) < 0)
-		DEBUG("%s: %s\n", "getsockopt SO_TIMESTAMP", strerror(errno));
-	else
-		DEBUG("SO_TIMESTAMP %d\n", val);
-
-	if (getsockopt(sock, SOL_SOCKET, SO_TIMESTAMPNS, &val, &len) < 0)
-		DEBUG("%s: %s\n", "getsockopt SO_TIMESTAMPNS",
-		       strerror(errno));
-	else
-		DEBUG("SO_TIMESTAMPNS %d\n", val);
-
-	if (getsockopt(sock, SOL_SOCKET, SO_TIMESTAMPING, &val, &len) < 0) {
-		DEBUG("%s: %s\n", "getsockopt SO_TIMESTAMPING",
-		       strerror(errno));
-	} else {
-		DEBUG("SO_TIMESTAMPING %d\n", val);
-		if (val != so_timestamping_flags)
-			DEBUG("   not the expected value %d\n",
-			       so_timestamping_flags);
-	}
-
-
-	// send packets forever every five seconds 
-	gettimeofday(&next, 0);
-	//next.tv_sec = (next.tv_sec + 1) ;
-	//next.tv_usec = 0;
-	while (1) {
-		struct timeval now;
-		struct timeval delta;
-		long delta_us;
-		int res;
-		fd_set readfs, errorfs;
-
-		gettimeofday(&now, 0);
-		delta_us = (long)(next.tv_sec - now.tv_sec) * 1000000 + (long)(next.tv_usec - now.tv_usec);
-
-		if(delta_us < 0){
-			// write one packet 
-			sendpacket(sock,
-				   (struct sockaddr *)&addr,
-				   sizeof(addr));
-			next.tv_sec = next.tv_sec + ((long)(1000+next.tv_usec))/1000000;
-			next.tv_usec = ((long)(1000+next.tv_usec))%1000000;
-			continue;
-		}
-	}
-	
-	return 0;
-}
-*/
