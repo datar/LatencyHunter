faf68948e2a7b60306a80fa4214159254520a562
datar <im@chenxing.me>
2016-03-17 09:37:56 -0700

    change from home

--- client.c
+++ client.c
@@ -8,18 +8,27 @@
 
 #define BUFFSIZE 255
 void Die(char *mess) { perror(mess); exit(1); }
- 
+
+int usage(){
+  fprintf(stderr, "USAGE: %s <server_ip> <port> <word>\n", argv[0]);
+  exit(1);
+}
+
+int set_socket_reuse(int sock){
+  int on = 1;
+  setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+  return 0;
+}
+
 int main(int argc, char *argv[]) {
   int sock;
   struct sockaddr_in echoserver;
-  struct sockaddr_in echoclient;
-  char buffer[BUFFSIZE];
-  unsigned int echolen, clientlen;
+  
+  unsigned int echolen;
   int received = 0;
 
   if (argc != 4) {
-    fprintf(stderr, "USAGE: %s <server_ip> <word> <port>\n", argv[0]);
-    exit(1);
+    usage();
   }
  
 
@@ -27,22 +36,27 @@ int main(int argc, char *argv[]) {
   if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
     Die("Failed to create socket");
   }
+  set_socket_reuse(sock);
+
   /* Construct the server sockaddr_in structure */
   memset(&echoserver, 0, sizeof(echoserver));       /* Clear struct */
   echoserver.sin_family = AF_INET;                  /* Internet/IP */
   echoserver.sin_addr.s_addr = inet_addr(argv[1]);  /* IP address */
-  echoserver.sin_port = htons(atoi(argv[3]));       /* server port */
+  echoserver.sin_port = htons(atoi(argv[2]));       /* server port */
   
 
   /* Send the word to the server */
-  echolen = strlen(argv[2]);
-  if (sendto(sock, argv[2], echolen, 0,
+  echolen = strlen(argv[3]);
+  if (sendto(sock, argv[3], echolen, 0,
              (struct sockaddr *) &echoserver,
              sizeof(echoserver)) != echolen) {
     Die("Mismatch in number of sent bytes");
   }
   
 
+  struct sockaddr_in echoclient;
+  char buffer[BUFFSIZE];
+  unsigned int echolen, clientlen;
   /* Receive the word back from the server */
   fprintf(stdout, "Received: ");
   clientlen = sizeof(echoclient);
